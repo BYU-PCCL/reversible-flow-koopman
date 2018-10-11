fixed loss bug validation logging bug changed variance of sample

datasets.py
@@ -4,11 +4,12 @@ import torch
 from torchvision import datasets, transforms
 
 class Dataset(datasets.MNIST):
-  def __init__(self, root='/tmp/mnist', download=True, overfit=False):
+  def __init__(self, root='/tmp/mnist', train=True, download=True, overfit=False):
   	self.overfit = overfit
   	super(Dataset, self).__init__(
   		root, 
   		download=download, 
+      train=train,
   		transform=transforms.Compose([
                            transforms.ToTensor(),
                            lambda x: torch.cat([x, x[0:1]], dim=0),

main.py
@@ -9,6 +9,7 @@ import trainer
 import inspect
 import libs.colors.colors as colors
 import utils
+import atexit
 
 args.module('model', models.ReversibleFlow)
 args.module('optimizer', torch.optim, default=torch.optim.Adam)
@@ -16,26 +17,26 @@ args.module('train_dataset', datasets)
 args.module('validation_dataset', datasets)
 args.module('trainer', trainer.Trainer)
 
-args.arguments(epochs=1, name='', batch_size=32, resume=False, resume_uid='', resume_tag='best', log_frequency=10, 
-               validation_frequency=5000, checkpoint_frequency=1000, cuda=True, print_model=False, max_grad=15)
+args.arguments(epochs=1, name='', batch_size=32, resume='', resume_uid='', log_frequency=10, 
+               validation_frequency=5000, checkpoint_frequency=1000, cuda=True, print_model=False, max_grad=40)
 
-args.defaults({'optimizer.lr': 1e-4})
+args.defaults({'optimizer.lr': 1e-3})
 
 pargs = args.reader()
 
 train_dataset = pargs.train_dataset()
-validation_dataset = pargs.validation_dataset()
+validation_dataset = pargs.validation_dataset(train=False)
 model = pargs.model(train_dataset)
 optimizer = pargs.optimizer(model.parameters())
-trainer = pargs.trainer(logname=pargs.stub())
+trainer = pargs.trainer(_logname=pargs.stub())
 
 if pargs.cuda:
   model.cuda()
   trainer.cuda()
 
-if pargs.resume:
+if len(pargs.resume) > 0:
   with utils.block('Resume', exit_on_error=True) as b:
-    path = trainer.resume(model, pargs.resume_tag, uid=pargs.resume_uid, unique=False)
+    path = trainer.resume(model, optimizer, pargs.resume, uid=pargs.resume_uid, unique=False)
     b.print('Successfully Resumed: {}'.format(path))
     b.print(trainer.state_dict)
     
@@ -53,51 +54,62 @@ with utils.block('Model') as b:
 
 @utils.profile
 def main():
+
+  # save a checkpoint at exit, regardless of the reason
+  atexit.register(trainer.checkpoint, model, optimizer, tag='exit', log=print)
+
   for (epoch, batch, steps, bar), data in trainer(train_dataset, epochs=pargs.epochs, progress='Training', shuffle=True, batch_size=pargs.batch_size):
+    
+    trainer.state_dict['step'] = steps + 1 # resume starting on the next step
+
     optimizer.zero_grad()
     model.train() if not model.training else None
     
     loss_value, out = model(*data)
     loss_value.backward()
 
-    if (steps + 1) % pargs.log_frequency == 0:
+    if steps % pargs.log_frequency == 0:
       trainer.log(steps,
                   loss=loss_value,
                   m=model.log(loss=loss_value, 
-                                  out=out,
-                                  data=data,
-                                  epoch=epoch,
-                                  batch=batch,
-                                  step=steps))
-
-    torch.nn.utils.clip_grad_value_(model.parameters(), pargs.max_grad)
-    optimizer.step()
-
-    if (steps + 1) % pargs.validation_frequency == 0:
+                              out=out,
+                              data=data,
+                              epoch=epoch,
+                              batch=batch,
+                              step=steps))
+    
+    if steps % pargs.validation_frequency == 0 and steps != 0:
       model.eval()
-      validation_loss = sum(model(*data)[0].item() for _, data in trainer(validation_dataset, batch_size=pargs.batch_size, progress='Validation', grad=False, leave=False)) 
-      validation_loss /= len(validation_dataset)
+      val_gen = trainer(validation_dataset, batch_size=pargs.batch_size, progress='Validation', grad=False, leave=False)
+      validation_loss = sum(model(*data)[0].item() for _, data in val_gen) / len(val_gen)
       trainer.log(steps, validation=validation_loss)
 
-      if validation_loss > trainer.state_dict.get('best_validation_loss', 0):
+      if validation_loss < trainer.state_dict.get('best_validation_loss', validation_loss + 1):
         trainer.state_dict['best_validation_loss'] = validation_loss
-        path = trainer.checkpoint(model, 'best')
-        bar.write('Checkpoint at {}: {}'.format(steps, path))
+        path = trainer.checkpoint(model, optimizer, tag='best_validation', log=bar.write)
 
-    if (steps + 1) % pargs.checkpoint_frequency == 0:
-      path = trainer.checkpoint(model, 'recent')
-      bar.write('Checkpoint at {}: {}'.format(steps, path))
+    if steps % pargs.checkpoint_frequency == 0  and steps != 0:
+      path = trainer.checkpoint(model, optimizer, tag='recent', log=bar.write)
+
+      trainer.state_dict['running_training_loss'] = .95 * trainer.state_dict.get('running_training_loss', loss_value) + .05 * loss_value
+      if trainer.state_dict['running_training_loss'] < trainer.state_dict.get('best_training_loss', 1e10):
+        trainer.state_dict['best_training_loss'] = trainer.state_dict['running_training_loss']
+        path = trainer.checkpoint(model, optimizer, tag='best_training', log=bar.write)
+
+    torch.nn.utils.clip_grad_norm_(model.parameters(), pargs.max_grad)
+    optimizer.step()
 
 main()
 
-# TODO: dynamic system dataset
 # TODO: dockerfile / DGX tests
 # TODO: multi-gpu integration for Trainer
-# TODO: handle "data:type=None" for args
+# TODO: dynamic system dataset
 # TODO: complex pointwise multiply / dynamic system inference
+
+# TODO: handle "data:type=None" for args
 # TODO: args README
 # TODO: args @argignore and @arginclude
-# TODO: experiment with normal_() again
+
 # TODO: experiment with a single dimension of data, the rest filled with zeros (low rank data)
 # TODO: why does negative scaling not seem to work as well?
 

models.py
@@ -85,32 +85,8 @@ class LogWhiteGaussian(nn.Module):
     super(LogWhiteGaussian, self).__init__()
 
   def forward(self, z):
-    log_likelihood = -0.5 * (LogWhiteGaussian.log_2pi + ((z ** 2).view(z.size(0), -1).mean(1)))
+    log_likelihood = -0.5 * (LogWhiteGaussian.log_2pi + ((z ** 2).view(z.size(0), -1).sum(1)))
     return log_likelihood
-
-# class FlowStep(Layer):
-#     """
-#         One step of flow described in paper
-#                       ▲
-#                       │
-#         ┌─────────────┼─────────────┐
-#         │  ┌──────────┴──────────┐  │
-#         │  │ flow coupling layer │  │
-#         │  └──────────▲──────────┘  │
-#         │             │             │
-#         │  ┌──────────┴──────────┐  │
-#         │  │  flow permutation   │  │
-#         │  │        layer        │  │
-#         │  └──────────▲──────────┘  │
-#         │             │             │
-#         │  ┌──────────┴──────────┐  │
-#         │  │     activation      │  │
-#         │  │ normalization layer │  │
-#         │  └──────────▲──────────┘  │
-#         └─────────────┼─────────────┘
-#                       │
-#                       │
-#     """
 
 class ReversibleFlow(nn.Module):
   """
@@ -191,7 +167,7 @@ class ReversibleFlow(nn.Module):
         z2 += shift
 
         inner_var_adjustment = (torch.log(z2.std()) + self.prior(z2.view(z2.size(0), -1)) * 1 / z2.std()) if self.inner_var_cond else 0
-        loglikelihood_accum += logscale.view(logscale.size(0), -1).mean(1) - inner_var_adjustment
+        loglikelihood_accum += logscale.view(logscale.size(0), -1).sum(1) - inner_var_adjustment
 
         z = torch.cat([z1, z2], dim=1)
 
@@ -231,7 +207,7 @@ class ReversibleFlow(nn.Module):
         z2 /= safescale
         
         inner_var_adjustment = torch.log(z2.std()) + 1 / z2.std() if self.inner_var_cond else 0
-        loglikelihood_accum -= logscale.view(logscale.size(0), -1).mean(1) - inner_var_adjustment
+        loglikelihood_accum -= logscale.view(logscale.size(0), -1).sum(1) - inner_var_adjustment
 
         z = torch.cat([z1, z2], dim=1)
 
@@ -252,23 +228,21 @@ class ReversibleFlow(nn.Module):
 
   @utils.profile
   def forward(self, x, y):
-    #x.normal_(0, 1)
-    x.fill_(0)
-    x[:, 0, 0, 0].normal_()
-
+    # x.normal_(0, 1)
+    # x.fill_(0)
+    # sx[:, 0, 0, 0].normal_()
     z, loglikelihood_accum = self.encode(x)
-
-    # flatten all the hierarchical layers
     zcat = torch.cat([x.view(x.size(0), -1) for x in z], dim=1)
 
-    n_bins = 2**8
-    M = np.prod(x.shape[1:])
-    discretization_correction = float(np.log(n_bins))
-    mean_log_likelihood = self.logprior(zcat) + .5 * loglikelihood_accum
-
-    loss = -mean_log_likelihood + discretization_correction
-    loss /=  float(np.log(2.))
-    loss = loss.mean()
+    M = float(np.prod(x.shape[1:]))
+    discretization_correction = float(np.log(2**8)) * M
+    mean_log_likelihood = self.logprior(zcat) + loglikelihood_accum
+    loss = (-mean_log_likelihood + discretization_correction) / M
+    
+    # the following command requires a sync operation
+    # it should be as low in this function as possible
+    loss = loss.mean() / float(np.log(2.))
+    
     assert not np.isnan(loss.item()), 'loss is nan'
     
     return loss, (zcat, z, loglikelihood_accum)
@@ -276,19 +250,20 @@ class ReversibleFlow(nn.Module):
   @utils.profile
   def log(self, data, step, out, **kwargs):
     self._logcount += 1
+    x, y = data
     zcat, z, loglikelihood_accum = out
 
     # TODO: remove this ASAP
+    stats = {}
     # stats.update({'gmax': max([p.grad.abs().max() for p in self.parameters()]),
     #               'gnorm': max([p.grad.abs().norm() for p in self.parameters()])})
-    stats = {}
-
+    
     if self._logcount % self.complex_log_frequency == 0:
 
-      stats = {'logdet': loglikelihood_accum.mean(),
+      stats = {'logdet': loglikelihood_accum.mean() / float(np.prod(x.shape[1:])),
                 'mean': zcat.mean(),
-                'std': zcat.std()}
-
+                'std': zcat.std(),
+                'var(std)': zcat.std(0).var()}
 
       # P = D.dot(D.T) / b
       # if not hasattr(self, '_running_cov_estimate'):
@@ -311,8 +286,9 @@ class ReversibleFlow(nn.Module):
       xhat, _ = self.decode(z) # Needs to be last, since decode works "inplace" on the input array
       if (xhat - data[0]).abs().max() > 1e-2:
         print('Reconstruction Error:', (xhat - data[0]).abs().max())
+        raise Exception('Reconstruction error is too high. Investigate.')
 
-      sample_z = [m.clone().normal_() * 0.7 for m in z]
+      sample_z = [m.clone().normal_() * m.std(0).unsqueeze(0) * .7 for m in z]
       [m[0].fill_(0) for m in sample_z]
       sample_images, _ = self.decode(sample_z)
 